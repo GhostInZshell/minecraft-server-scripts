49f2ee05741e040748e822b653c7116ff8d78b43
GhostInZshell <cheek-afraid-gray@duck.com>
2025-08-28 17:24:33 -0600

    Added colored prompts

--- backup.sh
+++ backup.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 ###### This file is version controlled in git ######
 
+# Colors
+YLW="\e[33m"
+GRN="\e[32m"
+RED="\e[31m"
+CYN="\e[36m"
+BLD="\e[1m"
+RESET="\e[0m"
+
 SRC="$HOME/paper_minecraft"
 DEST="$HOME/paper_backups_duplicity"
 TS=$(date +'%Y-%m-%d_%H-%M-%S')
 
-echo "[*] Starting backup at $TS..."
+echo "[${YLW}*${RESET}] Starting backup at $TS..."
 
 duplicity --no-encryption --progress backup "$SRC" file://"$DEST"
 
-echo "[*] Removing backups older than 90 days..."
+echo "[${YLW}*${RESET}] Removing backups older than 90 days..."
 
 duplicity remove-older-than 90D --force --no-encryption --progress file://"$DEST"
 
-echo "[✓] Backup complete."
+echo "[${GRN}✓${RESET}] Backup complete."
